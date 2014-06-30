94470538743e7e0474128c9e8af811c688691f9e
Jon Coffey <ghostlon@outlook.com>
2014-06-30 07:38:08 -0400

    updated with base16 colors

--- config.h
+++ config.h
@@ -6,12 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char font[] = "Source Code Pro:pixelsize=10";
-static int borderpx = 6;
+static int borderpx = 5;
 static char shell[] = "/bin/sh";
 
 /* Kerning / character bounding-box mutlipliers */
 float cwscale = 1.0;
-float chscale = 0.9;
+float chscale = 1.0;
 
 /*
  * word delimiter string
@@ -52,32 +52,31 @@ static unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#000000",
-	"#912226",
-	"#778900",
-	"#ae7b00",
-	"#1d2594",
-	"#682a9b",
-	"#2b6651",
-	"#929593",
-
-	/* 8 bright colors */
-	"#666666",
+	"#282a2e",
 	"#cc6666",
 	"#b5bd68",
 	"#f0c674",
 	"#81a2be",
-	"#b294bb",
+	"#a3685a",
 	"#8abeb7",
-	"#ecebec",
+	"#e0e0e0",
+
+	/* 8 bright colors */
+	"#1d1f21",
+	"#de935f",
+	"#373b41",
+	"#969896",
+	"#b4b7b4",
+	"#b294bb",
+	"#c5c8c6",
+	"#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 
-        "#c5c8c6",
-        "#1d1f21",
-        "#aeafad",
+        "#373b41",
+        "#ffffff",
 
 };
 
@@ -88,7 +87,7 @@ static const char *colorname[] = {
  */
 static unsigned int defaultfg = 256;
 static unsigned int defaultbg = 257;
-static unsigned int defaultcs = 258;
+static unsigned int defaultcs = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
